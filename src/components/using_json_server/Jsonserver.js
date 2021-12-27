4699bc10021173fa074c885f5690d58e44d4d136
ShaanJ20 <shaan.johari18@gmail.com>
2021-12-27 19:01:38 +0530

    Conditional loading

--- src/components/using_json_server/Jsonserver.js
+++ src/components/using_json_server/Jsonserver.js
@@ -5,21 +5,28 @@ import JsonServerOutput from './JsonServerOutput';
 export default function Jsonserver() {
 
     const [blogs, setblogs] = useState(null)
+    const [isPending, setisPending] = useState(true) // Conditional Loading
 
     useEffect(() =>{
-        fetch('http://localhost:8000/blogs/')
-        .then(res =>{
-            return res.json();
-        })
-        .then (data =>{
-            setblogs(data)
-            console.log(data)
-        });
+                fetch('http://localhost:8000/blogs/')
+                .then(res =>{
+                    return res.json();
+                })
+                .then (data =>{
+                    setblogs(data)
+                    console.log(data)
+        
+                    setisPending(false) // when the data comes it sets the laoding to fals
+            })
     },[]);
 
     return (
         <div className='blog'>
-            {blogs && <JsonServerOutput blog={blogs} />}
+            {/* Loading when is pending is true */}
+            {isPending && <div>Loading..</div>} 
+            {blogs ? <JsonServerOutput blog={blogs} /> : null}
+
         </div>
+
     )
 }
