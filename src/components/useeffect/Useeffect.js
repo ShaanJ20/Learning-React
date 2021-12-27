54123cbd234a87d9bd3fee514e833e0ec995e31b
ShaanJ20 <shaan.johari18@gmail.com>
2021-12-27 18:04:52 +0530

    Learning basics of useeffect

---
+++ src/components/useeffect/Useeffect.js
@@ -0,0 +1,15 @@
+import React, {useEffect} from 'react'
+
+
+export default function Useeffect() {
+    
+    useEffect(()=>{
+        console.log('use effect ran')
+    },[]);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
